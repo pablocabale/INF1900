d401f797dee190dbaee600021547f6cdc5743701
Pablo Cabale <pablo.cabale@polymtl.ca>
2023-01-17 18:55:12 -0500

    Correction TP1

--- pi.cpp
+++ pi.cpp
@@ -3,6 +3,12 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+enum class LedState {RED, GREEN, AMBER};
+
+bool detectButtonPress() {
+    return (PIND & (1 << PIND2));
+}
+
 void test_1 () {
     DDRC = 0x03;
 
@@ -24,24 +30,26 @@ void test_2 () {
     DDRD = 0x03;
 
     int cpt = 0;
+    bool couleurChange = false;
     for(;;){
         if (PIND & 0x04) {
             _delay_ms(10);
             if (PIND & 0x04){ 
-                if (cpt == 0) {
+                if (cpt == 0)
                     PORTD = 0x01;
-                }
-                if (cpt == 1) {
+                if (cpt == 1)
                     PORTD = 0x02;
-                } 
+                couleurChange = true;
             }
         }
         else {
             PORTD = 0x00;
-            cpt += 1;
-            if (cpt == 2) {
-                cpt = 0;
-            } 
+            if(couleurChange) {
+                cpt += 1;
+                if (cpt == 2)
+                    cpt = 0;
+                couleurChange = false;
+            }
         }
     }
     
